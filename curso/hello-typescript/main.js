1b1fd8c707216426a9a568006298525c79506b2c
@edualuc <edualuc@gmail.com>
2017-08-30 11:12:43 -0300

    inicial

---
+++ curso/hello-typescript/main.js
@@ -0,0 +1,11 @@
+var variavel = "variavel";
+function minhaFuncao(a, b) {
+    return a + b;
+}
+var num = 2;
+var PI = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (value) {
+    return value * 2;
+});
+numeros.map(function (valor) { return valor * 2; });
---
+++ curso/hello-typescript/main.ts
@@ -0,0 +1,15 @@
+var variavel = "variavel";
+
+function minhaFuncao( a, b) {
+    return a + b;
+}
+
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1,2,3]
+
+numeros.map( function (value) {
+    return value *2;
+});
+numeros.map(valor => valor *2 );
